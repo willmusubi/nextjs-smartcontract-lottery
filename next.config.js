c7247d587ba98721f61708d39a752ffe086c620b
willmusubi <willmusubi@gmail.com>
2023-03-06 22:58:16 -0800

    fixed non-static configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        loader: "imgix",
+        path: "",
+    },
+    nextConfig,
+};
